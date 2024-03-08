8e9c7014630e1d4156fbedfe7de7fef69440cfa9
Maves1 <seva21122@gmail.com>
2024-03-08 13:27:33 +0300

    [ml] add SM and SE models and test them

---
+++ ml/hmog/models/models.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy.spatial.distance import cityblock
+
+class SEClassifier:
+    def __init__(self) -> None:
+        pass
+
+    def fit(self, train_vector: np.ndarray):
+        self.mean_vector = np.mean(train_vector, axis=0)
+    
+    def decision_function(self, test_vector):
+        assert test_vector.shape[1] == self.mean_vector.shape[0]
+
+        return np.linalg.norm(test_vector - self.mean_vector, axis=1)
+
+class SMClassifier:
+    def __init__(self) -> None:
+        pass
+
+    def fit(self, train_vector: np.ndarray):
+        self.mean_vector = np.mean(train_vector, axis=0)
+    
+    def decision_function(self, test_vector):
+        assert test_vector.shape[1] == self.mean_vector.shape[0]
+
+        decision_scores = []
+
+        for i in range(test_vector.shape[0]):
+            decision_scores.append(cityblock(test_vector[i], self.mean_vector))
+
+        return np.array(decision_scores)
